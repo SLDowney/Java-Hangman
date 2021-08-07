9391b6d308a6be85261134fc43b8051f8fe0a30f
SLDowney <sdowney@outlook.com>
2021-08-07 18:04:45 -0230

    Game successfully gets user input, displays current letters above art, counts number of guesses, and displays right answer and appropriate lose art.

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,49 +1,167 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
     public static final int MAX_WRONG_GUESSES = 8;
-    private String wordToFind;
-    private char[] newWord;
-    private int currentNumErrors;
-    private String[] userLetters;
+    private static String currentWord = "";
+    private static char[] currentlettersArray;
+    private static int currentNumErrors;
+    public static int count = 0;
 
-    Random random = new Random();
-    Scanner scanner = new Scanner(System.in);
+    public static String getWord() {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        boolean invalid = false;
+        String userPick = "";
 
-    public String getWord() {
-        //Goes to HangmanArtWords to grab word from the words array using a random index
-        return HangmanArtWords.WORDS_ARRAY[random.nextInt(HangmanArtWords.WORDS_ARRAY.length)];
+        System.out.print("\nEnter 1 for Easy, 2 for Medium, or 3 for Hard: ");
+        int userChoice = scanner.nextInt();
+
+        while ( invalid == false )
+            if ( userChoice == 1 ) {
+                //Goes to HangmanArtWords to grab word from the words array using a random index
+                userPick = HangmanArtWords.WORDS_ARRAY_EASY[random.nextInt(HangmanArtWords.WORDS_ARRAY_EASY.length)];
+                invalid = true;
+            }
+            else if ( userChoice == 2 ) {
+                //Goes to HangmanArtWords to grab word from the words array using a random index
+                userPick =  HangmanArtWords.WORDS_ARRAY_MED[random.nextInt(HangmanArtWords.WORDS_ARRAY_MED.length)];
+                invalid = true;
+            }
+            else if ( userChoice == 3 ) {
+                //Goes to HangmanArtWords to grab word from the words array using a random index
+                userPick =  HangmanArtWords.WORDS_ARRAY_HARD[random.nextInt(HangmanArtWords.WORDS_ARRAY_HARD.length)];
+                invalid = true;
+            }
+            else {
+                //If userChoice is invalid, it will ask again
+                System.out.println("Invalid Entry");
+                invalid = false;
+            }
+        return userPick;
     }
 
-    public boolean userWinCheck() {
-        //boolean check to see if user wins. If true then user wins
-        return wordToFind.contentEquals(new String(newWord));
+    public static boolean userWinCheck() {
+        //boolean check to see if user letter guesses match the current word. If true then user wins
+        return currentWord.contentEquals(new String(currentlettersArray));
     }
 
-    public void newGame() {
-        int i;
+    public static void newGame() {
         //Reset error count to zero for new game
         currentNumErrors = 0;
         //Get new random word using getWord method
-        wordToFind = getWord();
-        //Reset all entries in userLetters array to blank (" ")
-        for ( i = 0; i < userLetters.length; i++) {
-            userLetters[i] = " ";
-        }
+        currentWord = getWord();
         //creates a char array with the length of the new random word, used to check/place user guesses
-        newWord = new char[newWord.length];
-        //Initializes all indexes in the newWord array to an underscore ("_"
-        for ( int j = 0; j < newWord.length; j++) {
-                newWord[j] = '_';
+        currentlettersArray = new char[currentWord.length()];
+        //Initializes all indexes in the newWord array to an underscore ("_")
+        for ( int j = 0; j < currentlettersArray.length; j++) {
+                if ( (String.valueOf(currentlettersArray[j])) == "-") {
+                    currentlettersArray[j] = '-';
+                }
+            currentlettersArray[j] = '_';
+        }
+        System.out.println("\n" + currentlettersArray);
+    }
+
+    public static void updateUserGuesses(String userEntry) {
+        if (currentWord.contains(userEntry)) {
+            //find the index of userEntry within the current word
+            int index = currentWord.indexOf(userEntry);
+
+            while (index >= 0) {
+                currentlettersArray[index] = userEntry.charAt(0);
+                index = currentWord.indexOf(userEntry, index + 1);
+            }
+        }
+        else {
+            //If userEntry is not in the current word, add 1 to the number of incorrect guesses
+            currentNumErrors++;
         }
     }
 
+    private static String displayUpdated() {
+        int i = 0;
+        //Class to add strings together to create larger strings
+        StringBuffer buffer = new StringBuffer();
 
+        while ( i < currentlettersArray.length ) {
+            buffer.append(currentlettersArray[i]);
+            //If i is equal to the last letter in the word
+            if ( i == currentlettersArray.length - 1 ) {
+                buffer.append(" ");
+            }
+            i++;
+        }
+        return buffer.toString();
+    }
 
+    public static void playGame() {
+        Scanner scanner = new Scanner(System.in);
 
+        while ( currentNumErrors < MAX_WRONG_GUESSES ) {
+            int guessesRamaining = MAX_WRONG_GUESSES - currentNumErrors;
+            if ( guessesRamaining == 1 ) {
+                System.out.print("Enter LAST Letter Guess: ");
+            }
+            else {
+                System.out.print("Enter Letter Guess: ");
 
+            }
+            String userEntry = scanner.next();
+            updateUserGuesses(userEntry);
+            //Shows current state of user guesses
+            System.out.println("\n" + displayUpdated());
+            userWinCheck();
+            if (userWinCheck() == true) {
+                System.out.println("\nWINNER!!");
+                System.out.println("You get the joy of being a winner at Hangman, you don't need any more art.");
+                break;
+            }
+            else {
+                System.out.println("Guesses remaining: " + guessesRamaining);
+                //switch statement to check number of current errors and print corresponding art
+                switch (currentNumErrors) {
+                    case 1 -> System.out.println(HangmanArtWords.stringArt1());
+                    case 2 -> System.out.println(HangmanArtWords.stringArt2());
+                    case 3 -> System.out.println(HangmanArtWords.stringArt3());
+                    case 4 -> System.out.println(HangmanArtWords.stringArt4());
+                    case 5 -> System.out.println(HangmanArtWords.stringArt5());
+                    case 6 -> System.out.println(HangmanArtWords.stringArt6());
+                    case 7 -> System.out.println(HangmanArtWords.stringArt7());
+                    case 8 -> System.out.println(HangmanArtWords.stringArt8());
+                }
+            }
+        }
+        if ( currentNumErrors >= MAX_WRONG_GUESSES ) {
+            count++;
+            System.out.println("\nYOU LOST HA HA!!! Word to find was: " + currentWord);
+            if ( count == 1 ) {
+                System.out.println(HangmanArtWords.lose1());
+            }
+            else if ( count == 2 ) {
+                System.out.println(HangmanArtWords.lose2());
+            }
+            else if ( count >= 3 ) {
+                System.out.println(HangmanArtWords.lose3());
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Hangman!");
+        System.out.print(HangmanArtWords.helloArt());
 
+        String userChoice = "y";
+        while (Objects.equals(userChoice, "y")) {
+            newGame();
+            playGame();
+            System.out.print("\nWould you like to play again? (y/n): ");
+            userChoice = scanner.next();
+        }
+        System.out.println("Thanks for playing!");
+        System.out.println(HangmanArtWords.byeArt());
+    }
 }
--- src/HangmanArtWords.java
+++ src/HangmanArtWords.java
@@ -1,5 +1,5 @@
 public class HangmanArtWords {
-    public static final String[] WORDS_ARRAY = {
+    public static final String[] WORDS_ARRAY_EASY = {
             "cat", "airplane", "Alligator", "ant", "apple", "baby",
             "ball", "balloon", "banana", "basketball", "bat", "beach",
             "bear", "bed", "bell", "bike", "bird", "blocks", "bone",
@@ -20,6 +20,63 @@ public class HangmanArtWords {
             "truck", "turtle", "water", "web", "whale", "worm"
     };
 
+    public static final String[] WORDS_ARRAY_MED = {
+            "taxi", "standing-ovation", "alarm clock", "tool", "banana-peel",
+            "flagpole", "money", "wallet", "ballpoint-pen", "sunburn",
+            "wedding-ring", "spy", "babysitter", "aunt", "acne", "bib",
+            "puzzle", "piece", "pawn", "astronaut", "tennis", "shoes",
+            "blue", "jeans", "twig", "outer", "space", "banister",
+            "batteries", "doghouse", "campsite", "plumber", "bedbug",
+            "throne", "tiptoe", "log", "mute", "pogo", "stick", "stoplight",
+            "ceiling-fan", "bedspread", "bite", "stove", "windmill", "nightmare",
+            "stripe", "spring", "wristwatch", "eat", "matchstick", "gumball",
+            "bobsled", "bonnet", "flock", "sprinkler", "living-room", "laugh",
+            "snuggle", "sneeze", "bud", "elf", "headache", "slam-dunk", "saddle",
+            "ironing-board", "bathroom", "scale", "kiss", "shopping-cart",
+            "shipwreck", "funny", "glide", "lamp", "candlestick", "grandfather",
+            "rocket", "home", "movies", "seesaw", "rollerblades", "smog", "grill",
+            "goblin", "coach", "claw", "cloud", "shelf", "recycle", "glue-stick",
+            "Christmas", "front-porch", "robot", "foil", "rib", "robe", "crumb",
+            "paperback", "hurdle", "rattle", "fetch", "date", "iPod", "dance",
+            "cello", "flute", "dock", "prize", "dollar", "puppet", "brass", "firefighter",
+            "huddle", "easel", "pigpen", "bunk-bed", "bowtie", "fiddle", "dentist",
+            "baseboards", "letter-opener", "photographer", "magic", "Old-Spice", "monster"
+    };
+
+    public static final String[] WORDS_ARRAY_HARD = {
+            "abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom",
+            "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini",
+            "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo",
+            "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph",
+            "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri",
+            "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip",
+            "espionage", "exodus", "faking", "fishhook", "fixable", "fjord",
+            "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled",
+            "frizzled", "fuchsia", "funny", "gabby", "galaxy", "galvanize", "gazebo",
+            "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip",
+            "grogginess", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox",
+            "injury", "ivory", "ivy", "jackpot", "jaundice", "jawbreaker", "jaywalk",
+            "jazziest", "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey",
+            "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo",
+            "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch",
+            "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury",
+            "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify",
+            "naphtha", "nightclub", "nowadays", "numbskull", "nymph", "onyx", "ovary",
+            "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel", "pizazz",
+            "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz",
+            "queue", "quips", "quixotic", "quiz", "quizzes", "quorum", "razzmatazz",
+            "rhubarb", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy",
+            "sphinx", "spritz", "squawk", "staff", "strength", "strengths", "stretch",
+            "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless",
+            "thumbscrew", "topaz", "transcript", "transgress", "transplant",
+            "twelfth", "unknown", "unworthy", "unzip", "uptown", "vaporize",
+            "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway", "waltz", "wave",
+            "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever",
+            "wimpy", "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone",
+            "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag",
+            "zigzagging", "zilch", "zipper", "zodiac", "zombie"
+    };
+
     public static String stringArt1() {
         String art1 = "+---+\n" +
                 "  |   |\n" +
@@ -27,10 +84,10 @@ public class HangmanArtWords {
                 "      |\n" +
                 "      |\n" +
                 "      |\n" +
-                "=========''', '''";
+                "=============";
         return art1;
     }
-    public static String stringArtt2() {
+    public static String stringArt2() {
 
         String art2 = "+---+\n" +
                 "  |   |\n" +
@@ -38,7 +95,7 @@ public class HangmanArtWords {
                 "      |\n" +
                 "      |\n" +
                 "      |\n" +
-                "=========''', '''";
+                "=============";
         return art2;
     }
 
@@ -50,7 +107,7 @@ public class HangmanArtWords {
                 "  |   |\n" +
                 "      |\n" +
                 "      |\n" +
-                "=========''', '''";
+                "=============";
         return art3;
     }
 
@@ -61,7 +118,7 @@ public class HangmanArtWords {
                 " /|   |\n" +
                 "      |\n" +
                 "      |\n" +
-                "=========''', '''";
+                "=============";
         return art4;
     }
     public static String stringArt5() {
@@ -72,7 +129,7 @@ public class HangmanArtWords {
                 " /|\\  |\n" +
                 "      |\n" +
                 "      |\n" +
-                "=========''', '''";
+                "=============";
         return art5;
     }
     public static String stringArt6() {
@@ -83,7 +140,7 @@ public class HangmanArtWords {
                 " /|\\  |\n" +
                 " /    |\n" +
                 "      |\n" +
-                "=========''', '''";
+                "=============";
         return art6;
     }
     public static String stringArt7() {
@@ -94,7 +151,7 @@ public class HangmanArtWords {
                 " /|\\  |\n" +
                 " / \\  |\n" +
                 "      |\n" +
-                "========='''";
+                "=============";
         return art7;
     }
 
@@ -106,7 +163,107 @@ public class HangmanArtWords {
                 " /|\\  |\n" +
                 " / \\  |\n" +
                 "      |\n" +
-                "========='''";
+                "=============";
         return art8;
     }
+
+    public static String lose1() {
+        String loseArt1 = "    .-\"\"\"\"\"\"-.\n" +
+                "   .'          '.\n" +
+                "  /   O      O   \\\n" +
+                " :           `    :\n" +
+                " |                |\n" +
+                " :    .------.    :\n" +
+                "  \\  '        '  /\n" +
+                "   '.          .'\n" +
+                "     '-......-'";
+    return loseArt1;
+    }
+
+    public static String lose2() {
+        String loseArt2 = " |\\ | |  ||\\ \\ /(_~     |~)|_~|\\/||_~|\\/||~)|_~|~)\n" +
+                "     |~\\|_|/\\||~\\ | ,_)     |~\\|__|  ||__|  ||_)|__|~\\\n" +
+                "\n" +
+                "        \\ //~\\| |    |\\ |~)|_~    | ||\\ ||/~\\| ||_~\n" +
+                "         | \\_/\\_/    |~\\|~\\|__    \\_/| \\||\\_X\\_/|__\n" +
+                "\n" +
+                "      (J U S T   L I K E   E V E R Y O N E   E L S E)\n" +
+                "      _____         _____         _____         _____\n" +
+                "    .'     '.     .'     '.     .'     '.     .'     '.\n" +
+                "   /  o   o  \\   /  o   o  \\   /  o   o  \\   /  o   o  \\\n" +
+                "  |           | |           | |           | |           |\n" +
+                "  |  \\     /  | |  \\     /  | |  \\     /  | |  \\     /  |\n" +
+                "   \\  '---'  /   \\  '---'  /   \\  '---'  /   \\  '---'  /\n" +
+                "    '._____.'     '._____.'     '._____.'     '._____.'\n" +
+                "      _____         _____         _____         _____\n" +
+                "    .'     '.     .'     '.     .'     '.     .'     '.\n" +
+                "   /  o   o  \\   /  o   o  \\   /  o   o  \\   /  o   o  \\\n" +
+                "  |           | |           | |           | |           |\n" +
+                "  |  \\     /  | |  \\     /  | |  \\     /  | |  \\     /  |\n" +
+                "   \\  '---'  /   \\  '---'  /   \\  '---'  /   \\  '---'  /\n" +
+                "    '._____.'     '._____.'     '._____.'     '._____.'\n" +
+                "      _____         _____         _____         _____\n" +
+                "    .'     '.     .'     '.     .'     '.     .'     '.\n" +
+                "   /  o   o  \\   /  o   o  \\   /  o   o  \\   /  o   o  \\\n" +
+                "  |           | |           | |           | |           |\n" +
+                "  |  \\     /  | |  \\     /  | |  \\     /  | |  \\     /  |\n" +
+                "   \\  '---'  /   \\  '---'  /   \\  '---'  /   \\  '---'  /\n" +
+                "    '._____.'     '._____.'     '._____.'     '._____.'\n" +
+                "      _____         _____         _____         _____\n" +
+                "    .'     '.     .'     '.     .'     '.     .'     '.\n" +
+                "   /  o   o  \\   /  o   o  \\   /  o   o  \\   /  o   o  \\\n" +
+                "  |           | |           | |           | |           |\n" +
+                "  |  \\     /  | |  \\     /  | |  \\     /  | |  \\     /  |\n" +
+                "   \\  '---'  /   \\  '---'  /   \\  '---'  /   \\  '---'  /\n" +
+                "    '._____.'     '._____.'     '._____.'     '._____.'\n" +
+                "      _____         _____         _____         _____\n" +
+                "    .'     '.     .'     '.     .'     '.     .'     '.\n" +
+                "   /  o   o  \\   /  o   o  \\   /  o   o  \\   /  o   o  \\\n" +
+                "  |           | |           | |           | |           |\n" +
+                "  |  \\     /  | |  \\     /  | |  \\     /  | |  \\     /  |\n" +
+                "   \\  '---'  /   \\  '---'  /   \\  '---'  /   \\  '---'  /\n" +
+                "    '._____.'     '._____.'     '._____.'     '._____.'";
+        return loseArt2;
+    }
+
+    public static String lose3() {
+        String loseArt3 = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'               `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  \n" +
+                "$$$$$$$$$$$$$$$$$$$$$$$$$$$$'                   `$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n" +
+                "$$$'`$$$$$$$$$$$$$'`$$$$$$!                       !$$$$$$'`$$$$$$$$$$$$$'`$$$\n" +
+                "$$$$  $$$$$$$$$$$  $$$$$$$                         $$$$$$$  $$$$$$$$$$$  $$$$\n" +
+                "$$$$. `$' \\' \\$`  $$$$$$$!                         !$$$$$$$  '$/ `/ `$' .$$$$\n" +
+                "$$$$$. !\\  i  i .$$$$$$$$                           $$$$$$$$. i  i  /! .$$$$$\n" +
+                "$$$$$$   `--`--.$$$$$$$$$                           $$$$$$$$$.--'--'   $$$$$$\n" +
+                "$$$$$$L        `$$$$$^^$$                           $$^^$$$$$'        J$$$$$$\n" +
+                "$$$$$$$.   .'   \"\"~   $$$    $.                 .$  $$$   ~\"\"   `.   .$$$$$$$\n" +
+                "$$$$$$$$.  ;      .e$$$$$!    $$.             .$$  !$$$$$e,      ;  .$$$$$$$$\n" +
+                "$$$$$$$$$   `.$$$$$$$$$$$$     $$$.         .$$$   $$$$$$$$$$$$.'   $$$$$$$$$\n" +
+                "$$$$$$$$    .$$$$$$$$$$$$$!     $$`$$$$$$$$'$$    !$$$$$$$$$$$$$.    $$$$$$$$\n" +
+                "$JT&yd$     $$$$$$$$$$$$$$$$.    $    $$    $   .$$$$$$$$$$$$$$$$     $by&TL$\n" +
+                "                                 $    $$    $\n" +
+                "                                 $.   $$   .$\n" +
+                "                                 `$        $'\n" +
+                "                                  `$$$$$$$$'";
+        return loseArt3;
+    }
+
+    public static String byeArt() {
+        String byeArt = "| |               \n" +
+                "| |__  _   _  ___ \n" +
+                "| '_ \\| | | |/ _ \\\n" +
+                "| |_) | |_| |  __/\n" +
+                "|_.__/ \\__, |\\___|\n" +
+                "        __/ |     \n" +
+                "       |___/ ";
+    return byeArt;
+    }
+
+    public static String helloArt() {
+        String helloArt = "┏┓┏┳━━┳┓┏┓┏━━━━┓\n" +
+                "┃┗┛┃━━┫┃┃┃┃ ╭╮ ┃\n" +
+                "┃┏┓┃━━┫┃┫┗┫ ╰╯ ┃\n" +
+                "┗┛┗┻━━┻━┻━┻━━━━┛";
+        return helloArt;
+    }
 }
+
